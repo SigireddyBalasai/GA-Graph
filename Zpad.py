10e4d185e8e57b0c92dbed468a57a15ff19fb81b
Your Name <you@example.com>
2023-12-21 19:08:47 +0530

    ok

--- Zpad.py
+++ Zpad.py
@@ -6,23 +6,18 @@ class ZeroPadConcatLayer(tf.keras.layers.Layer):
         super(ZeroPadConcatLayer, self).__init__(**kwargs)
 
     def call(self, inputs):
-        # pad along 2 and 3 along the height and width and concatenate along depth
+        # resize along 2 and 3 along the height and width and concatenate along depth
         # inputs is a list of tensors
-        # all tensors needs to be padded based on the max height and width
+        # all tensors needs to be resized based on the max height and width
         # then concatenate along depth
         # return the concatenated tensor
 
         # get the max height and width
         max_width = np.max([x.shape[1] for x in inputs])
         max_height = np.max([x.shape[2] for x in inputs])
-        # pad all the tensors
-        padded_inputs = [tf.pad(x, [[0, 0], [0, max_width - x.shape[1]], [0, max_height - x.shape[2]], [0, 0]]) for x in inputs]
+        # resize all the tensors
+        resized_inputs = [tf.image.resize(x, [max_width, max_height]) for x in inputs]
         # concatenate along depth
-        concatenated_tensor = tf.concat(padded_inputs, axis=3)
+        concatenated_tensor = tf.concat(resized_inputs, axis=3)
         #print(concatenated_tensor.shape, "concatenated_tensor.shape", max_width, max_height, "max_width, max_height", [x.shape for x in inputs], "inputs")
         return concatenated_tensor
-    
-    
-
-
-
